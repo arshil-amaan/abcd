196337b533616eacaa089170f47a7570c9fe8553
Arshil <arshilamaanansari@gmail.com>
2024-08-23 21:09:19 +0530

    starting to style

--- src/Components/Projects2/Projects2.jsx
+++ src/Components/Projects2/Projects2.jsx
@@ -5,6 +5,7 @@ import './styles.css'
 const Projects2 = () => {
     return (
         <div id="projects-section" className="center">
+            <h2 className='text-3xl font-bold text-center mb-12'>Projects</h2>
             <Slider data={data} activeSlide={2} />
         </div>
     )
