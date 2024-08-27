3951ba46bec22bcc90aad1f13b2a7a1a885cf0dc
Arshil <arshilamaanansari@gmail.com>
2024-08-27 17:48:42 +0530

    fix slider buttons

--- src/Components/Projects2/Slider.jsx
+++ src/Components/Projects2/Slider.jsx
@@ -83,7 +83,7 @@ export default (props) => {
             </div>
             {/* carousel */}
 
-            <div className="btns flex justify-center items-center">
+            <div className="btns flex justify-center items-center z-20">
                 <div
                     className="btn"
                     onClick={prev}
