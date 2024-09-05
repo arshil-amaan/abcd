f78440303fbf07aab8286446a651958ed8d2d273
Arshil <arshilamaanansari@gmail.com>
2024-09-06 00:09:27 +0530

    fix: responsive skills

--- src/assets/UI/Skills/DesktopSkills.jsx
+++ src/assets/UI/Skills/DesktopSkills.jsx
@@ -9,22 +9,8 @@ const DesktopSkills = () => {
     const techList2 = allTechList.slice(midIndex);
     return (
         <div className='w-full'>
-                    <SkillCard list={techList}/>
-                    <SkillCard list={techList2} direction="left"/>
-            {/* <Suspense fallback={<div>Loading skills...</div>}>
-                <Marquee speed={60} direction="" className=" pt-24" pauseOnHover style={marqueeStyles} >
-                    {techList.map((item,key)=>{
-                        return (
-<div>{item.tech} hello</div>
-                        )
-                    })}
-                </Marquee>
-            </Suspense> */}
-            {/* <Suspense fallback={<div>Loading skills...</div>}>
-                <Marquee speed={60} className="pt-20" pauseOnHover style={marqueeStyles}>
-                    {techList2.map((tech, key) => <SkillCard key={key} tech={tech.tech} Icon={tech.Icon} tooltip={tech.tooltip} />)}
-                </Marquee>
-            </Suspense> */}
+            <SkillCard list={techList} />
+            <SkillCard list={techList2} direction="left" />
         </div>
     )
 }
